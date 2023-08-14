0c197484d0d63e8dd947eae6fbd373e25d8c6f1a
Joaofsrs <rsjoao12@gmail.com>
2023-08-14 14:23:08 -0300

    acesso pelo github token

--- src/services/authApi.js
+++ src/services/authApi.js
@@ -4,4 +4,9 @@ export async function signIn(email, password) {
   const response = await api.post('/auth/sign-in', { email, password });
   return response.data;
 }
+
+export async function signInGit(code) {
+  const response = await api.post('/auth/sign-in-git', { code });
+  return response.data;
+}
 //
